4672289ab9d6bc1ce254d87c6cdd8ee4341ed5fb
thanh309 <vuthnh.td@gmail.com>
2024-11-12 12:46:16 +0700

    more checkpoint

--- code/scrape_rating.py
+++ code/scrape_rating.py
@@ -7,7 +7,23 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
+import requests
+import random
+
+
+USER_AGENTS = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0',
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/109.0',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.5790.170 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_0) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Safari/605.1.15'
+]
+UID = ('16619373', '0307374', '120934913')  # 290, 631, private
 ADBLOCK_PATH = 'resources/adblock.crx'
+URL = f'https://www.imdb.com/user/ur{UID[0]}/ratings/?view=compact&title_type=feature&sort=num_votes%2Cdesc'
+
 
 def create_driver() -> Chrome:
     options = ChromeOptions()
@@ -17,10 +33,9 @@ def create_driver() -> Chrome:
     driver = Chrome(options=options)
     return driver
 
+
 driver = create_driver()
-driver.get(
-    'https://www.imdb.com/user/ur16619373/ratings/?view=compact&title_type=feature&sort=num_votes%2Cdesc'
-)
+driver.get(URL)
 
 sleep(30)
 driver.quit()
